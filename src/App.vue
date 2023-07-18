56c66f7a542f0b7b287bef6574b628c5b4292e5e
rmaulanatj <robbimaulanatj17@gmail.com>
2023-07-18 23:25:25 +0700

    rename

--- src/App.vue
+++ src/App.vue
@@ -2,33 +2,61 @@
   <!-- component -->
   <!-- This is an example component -->
   <div class="max-w-7xl mx-auto flex flex-col relative">
-
-    <nav class="max-w-7xl px-5 md:fixed top-0 z-[98] w-screen backdrop-blur-md bg-[#121212] bg-opacity-80">
-      <div class="container mx-auto flex flex-wrap items-center justify-between">
+    <nav
+      class="max-w-7xl px-5 md:fixed top-0 z-[98] w-screen backdrop-blur-md bg-[#121212] bg-opacity-80"
+    >
+      <div
+        class="container mx-auto flex flex-wrap items-center justify-between"
+      >
         <button @click="redirectToHome" class="flex">
-          <span class="self-center text-lg text-[#ffdb70] font-semibold whitespace-nowrap fadein-bot hover:text-amber-100">bagasRakha();</span>
+          <span
+            class="self-center text-lg text-[#ffdb70] font-semibold whitespace-nowrap fadein-bot hover:text-amber-100"
+            >RobbiMaulana();</span
+          >
         </button>
         <div class="flex md:order-2 fadein-bot">
-          <a href="https://github.com/rakha-elctrnx"><img class="w-9 rounded-full" src="https://seeklogo.com/images/G/github-logo-2E3852456C-seeklogo.com.png" alt="github"></a>
+          <a href="https://github.com/rakha-elctrnx"
+            ><img
+              class="w-9 rounded-full"
+              src="https://seeklogo.com/images/G/github-logo-2E3852456C-seeklogo.com.png"
+              alt="github"
+          /></a>
         </div>
-        <div class="hidden md:flex justify-between items-center w-full md:w-auto md:order-1" id="mobile-menu-3">
-          <ul class="flex-col md:flex-row flex md:space-x-8 mt-4 md:mt-0 md:text-sm md:font-medium">
+        <div
+          class="hidden md:flex justify-between items-center w-full md:w-auto md:order-1"
+          id="mobile-menu-3"
+        >
+          <ul
+            class="flex-col md:flex-row flex md:space-x-8 mt-4 md:mt-0 md:text-sm md:font-medium"
+          >
             <li>
-              <router-link to="/"
+              <router-link
+                to="/"
                 class="fadein-bot text-gray-700 hover:bg-gray-50 border-b border-gray-100 md:hover:bg-transparent md:border-0 block pl-3 pr-4 py-2 md:hover:text-gray-500 md:p-0"
-                aria-current="page">Home</router-link>
+                aria-current="page"
+                >Home</router-link
+              >
             </li>
             <li>
-              <router-link to="/about"
-                class="fadein-bot fadein-1 text-gray-700 hover:bg-gray-50 border-b border-gray-100 md:hover:bg-transparent md:border-0 block pl-3 pr-4 py-2 md:hover:text-gray-500 md:p-0">About</router-link>
+              <router-link
+                to="/about"
+                class="fadein-bot fadein-1 text-gray-700 hover:bg-gray-50 border-b border-gray-100 md:hover:bg-transparent md:border-0 block pl-3 pr-4 py-2 md:hover:text-gray-500 md:p-0"
+                >About</router-link
+              >
             </li>
             <li>
-              <router-link to="/portfolio"
-                class="fadein-bot fadein-2 text-gray-700 hover:bg-gray-50 border-b border-gray-100 md:hover:bg-transparent md:border-0 block pl-3 pr-4 py-2 md:hover:text-gray-500 md:p-0">Portfolio</router-link>
+              <router-link
+                to="/portfolio"
+                class="fadein-bot fadein-2 text-gray-700 hover:bg-gray-50 border-b border-gray-100 md:hover:bg-transparent md:border-0 block pl-3 pr-4 py-2 md:hover:text-gray-500 md:p-0"
+                >Portfolio</router-link
+              >
             </li>
             <li>
-              <router-link to="/blog"
-                class="fadein-bot fadein-3 text-gray-700 hover:bg-gray-50 border-b border-gray-100 md:hover:bg-transparent md:border-0 block pl-3 pr-4 py-2 md:hover:text-gray-500 md:p-0 blog">Blog</router-link>
+              <router-link
+                to="/blog"
+                class="fadein-bot fadein-3 text-gray-700 hover:bg-gray-50 border-b border-gray-100 md:hover:bg-transparent md:border-0 block pl-3 pr-4 py-2 md:hover:text-gray-500 md:p-0 blog"
+                >Blog</router-link
+              >
             </li>
           </ul>
         </div>
@@ -39,24 +67,34 @@
       <router-view />
     </div>
   </div>
-  <footer class="block md:hidden fixed bottom-0 left-0 right-0 rounded-t-3xl border border-[#383838] bg-[#121212] bg-opacity-80 backdrop-blur-md backdrop-opacity-90">
-  <nav class="flex justify-around py-4 text-xs">
-    <router-link to="/" class="text-gray-300 hover:text-white">Home</router-link>
-    <router-link to="/about" class="text-gray-300 hover:text-white">About</router-link>
-    <router-link to="/portfolio" class="text-gray-300 hover:text-white">Portfolio</router-link>
-    <router-link to="/blog" class="text-gray-300 hover:text-white">Blog</router-link>
-  </nav>
-</footer>
+  <footer
+    class="block md:hidden fixed bottom-0 left-0 right-0 rounded-t-3xl border border-[#383838] bg-[#121212] bg-opacity-80 backdrop-blur-md backdrop-opacity-90"
+  >
+    <nav class="flex justify-around py-4 text-xs">
+      <router-link to="/" class="text-gray-300 hover:text-white"
+        >Home</router-link
+      >
+      <router-link to="/about" class="text-gray-300 hover:text-white"
+        >About</router-link
+      >
+      <router-link to="/portfolio" class="text-gray-300 hover:text-white"
+        >Portfolio</router-link
+      >
+      <router-link to="/blog" class="text-gray-300 hover:text-white"
+        >Blog</router-link
+      >
+    </nav>
+  </footer>
 </template>
 
 <script>
 export default {
   methods: {
     redirectToHome() {
-      this.$router.push('/')
-    }
+      this.$router.push("/");
+    },
   },
-}
+};
 </script>
 
 <style>
@@ -83,10 +121,12 @@ export default {
   border-radius: 5px;
 }
 
-::-webkit-scrollbar-button { width: 20px; }
+::-webkit-scrollbar-button {
+  width: 20px;
+}
 
 body {
-  font-family: 'Poppins', sans-serif;
+  font-family: "Poppins", sans-serif;
   background: hsl(0, 0%, 7%);
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
@@ -126,7 +166,6 @@ nav a.router-link-exact-active:hover {
   color: white;
 }
 
-
 @keyframes fadeInLeft {
   0% {
     opacity: 0;
@@ -137,5 +176,4 @@ nav a.router-link-exact-active:hover {
     transform: translateX(0);
   }
 }
-
 </style>
